e2db07f5bb16b431d19edee5a3e094d995579f27
Riley Scheid & Mila <pair+Sp33drac3r.ftbessmann@devbootcamp.com>
2016-04-18 13:50:03 -0700

    Make improvements to directed_graph.rb Deprecate old directed graph file Improvements include:   Automatic index creation   No edges hash. Everything is contained within the nodes hash.

--- directed_graph.rb
+++ directed_graph.rb
@@ -1,32 +1,27 @@
 class DirectedGraph #< Graph
 
-  Node = Struct.new(:value)
-
-  attr_accessor :edges
+  attr_accessor :nodes
 
   def initialize
     @nodes = {}
-    @edges = {}
   end
 
-  def add(value, node_id, edges=Array.new)
-    node = Node.new(value)
-    @nodes[node_id] = node
-    @edges[node_id] = edges
+  def add(value, edges=[])
+    node = Node.new(value, edges)
+    @nodes[node.id] = node
     self
   end
 
   def delete(node_id)
-    node = @nodes[node_id]
-    @nodes[node_id] = nil
-    @edges.delete(node)
+    @nodes.delete(node_id)
   end
 
-  def delete_edge(node_id, *edges)\
-    alledges = @edges[node_id]
-    edges.each {|edge| alledges.delete(edge)}
+  def delete_edge(node_id, *edges_to_delete)\
+    alledges = @nodes[node_id].edges
+    edges_to_delete.each {|edge| alledges.delete(edge)}
   end
 
+  #done
   def get_node_value(node_id)
     @node[node_id].value
   end
@@ -35,30 +30,43 @@ class DirectedGraph #< Graph
     @nodes[node_id].value = value
   end
 
-  def add_edge(x, y)
-    @edges[x] << y
+  def add_edge(node_id, edge)
+    @nodes[node_id].edges << edge
   end
 
-  def adjacent?(x, y)
-    return true if @edges[x].include?(y) || @edges[y].include?(x)
-    false
+  def adjacent?(node_id, edge)
+    @nodes[node_id].edges.include?(edge)
   end
 
   def neighbours(node_id)
-    @edges[node_id]
+    @nodes[node_id].edges
   end
 
   def to_s
     s = ""
     @nodes.each do |node_id, node|
-      s += "#{node_id} (#{node.value}) => #{@edges[node_id]} \n"
+      s += "#{node_id} (#{node.value}) => #{node.edges} \n"
     end
     return s
   end
 end
 
+class Node
+  @@node = 0
+  attr_accessor :value, :edges
+  attr_reader :id
+
+  def initialize(value, edges)
+    @value = value
+    @edges = edges
+    @id = @@node
+    @@node += 1
+  end
+end
+
 graph = DirectedGraph.new
-graph.add("James", 1).add("Tim", 2, [1]).add("Tom", 3, [1])
-graph.add_edge(1, 3)
+graph.add("James").add("Tim", [1]).add("Tom", [1])
+graph.add_edge(0, 2)
+graph.add_edge(1, 2)
 puts graph.to_s
 puts graph.adjacent?(1, 2)
