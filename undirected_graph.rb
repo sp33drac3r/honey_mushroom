71ceb5b5bdfde34adc447a8a2b4ecc68981af0f9
Riley Scheid <riley.k.scheid@gmail.com>
2016-04-20 18:34:00 -0700

    Try to fix delete node

--- undirected_graph.rb
+++ undirected_graph.rb
@@ -10,7 +10,8 @@ class UndirectedGraph < Graph
   end
 
   def delete_node(node_id)
-    @nodes[node_id].edges.each { |edge| delete_edge(edge, node_id) }
+    all_edges = @nodes[node_id].edges
+    all_edges.each { |edge| delete_edge(node_id, edge) }
     super
   end
 
@@ -20,15 +21,18 @@ class UndirectedGraph < Graph
     @nodes[edge].edges << node_id
   end
 
-  def delete_edge(node_id, *edges_to_delete)
-    super
-    edges_to_delete.each { |edge| @nodes[edge].edges.delete(node_id) }
+  def delete_edge(node_id, edge)
+    @nodes[node_id].edges.delete(edge)
+    @nodes[edge].edges.delete(node_id)
+    # edges_to_delete.each { |edge| @nodes[node_id].edges.delete(edge) }
+    # edges_to_delete.each { |edge| @nodes[edge].edges.delete(node_id) }
+    # super
   end
 end
 
 graph = UndirectedGraph.new
 
-10.times {graph.add_node(nil)}
+7.times {graph.add_node(nil)}
 
 File.open("test.txt").each_line do |line|
   node_id = line.split(' ')[0].to_i
@@ -36,11 +40,17 @@ File.open("test.txt").each_line do |line|
   graph.add_edge(node_id, edge)
 end
 
-puts graph.nodes
-puts graph.nodes[2].edges
+# puts graph.nodes
+# puts graph.nodes[2].edges
+# graph.nodes[2].edges.each { |edge| graph.delete_edge(edge, 2) }
+# graph.delete_edge(2, 0)
+# graph.delete_edge(2, 5)
+# graph.delete_edge(2, 6)
+
+
 graph.delete_node(2)
 puts graph.nodes
 
-node_id = 0
-target_node = 6
-puts "Breadth first: #{graph.breadth_first_search(node_id, target_node)}"
+# node_id = 0
+# target_node = 6
+# puts "Breadth first: #{graph.breadth_first_search(node_id, target_node)}"
