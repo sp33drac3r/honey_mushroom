19fcf498521ccd819d81168f78fd06110ec66d8c
Riley Scheid <riley.kenyon@gmail.com>
2016-05-01 09:12:12 -0700

    Binary search tree insert working

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -4,6 +4,10 @@ class BinarySearchTree
     @head = Node.new(value)
   end
 
+  def set_head(value)
+    @head = Node.new(value)
+  end
+
   def search(node=@head, value)
     if node.value == value
       return node.value
@@ -12,23 +16,44 @@ class BinarySearchTree
     else
       return search(value.right, value)
     end
+    return nil
+  end
+
+  def include?(node=@head, value)
+    !!search(node, value)
   end
 
   def insert(node=@head, value)
-    if node.value < value && node.left.nil?
-      node.left = Node.new(value)
-    elsif node.value > value && node.left.nil?
-      node.right = Node.new(value)
-    elsif node.value < value
-      insert(node.right, value)
+    if node.value < value
+      insert_right(node.right, value)
+    elsif node.value > value
+      insert_left(node.left, value)
     else
+      return false
+    end
+  end
+
+  private
+
+  def insert_left(node, value)
+    if self.left
       insert(node.left, value)
+    else
+      self.left = Node.new(value)
+    end
+  end
+
+  def insert_right(node, value)
+    if self.right
+      right.insert(node.right,  value)
+    else
+      self.right = Node.new(value)
     end
   end
 end
 
 class Node
-  def initialize(value=nil)
+  def initialize(value)
     @value = value
     @right = nil
     @left = nil
