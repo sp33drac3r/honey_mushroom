095a66367fd64ee2ae22a6b23081bfe1c662179f
Riley Scheid <riley.kenyon@gmail.com>
2016-07-09 19:56:30 -0700

    Fix typos in rspec"

---
+++ spec/binary_search_tree_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../lib/honey_mushroom/binary_search_tree'
+
+
+#TODO Add tests for binary search tree
+describe BinarySearchTree do
+  context 'is a set of unique values in an ordered tree structure' do
+    before(:each) do
+      #create a viable binary search tree
+    end
+
+    it 'inserts a node at a leaf' do
+    end
+
+    it 'returns a node that is in the tree' do
+    end
+
+    it 'knows if a value is within its tree' do
+    end
+
+    it 'does not show false positive for value that is not in tree' do
+    end
+
+    it 'deletes a leaf node' do
+    end
+
+    it 'deletes a central node' do
+    end
+  end
+end
